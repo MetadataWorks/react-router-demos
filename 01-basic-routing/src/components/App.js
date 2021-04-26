ac6dd6c36c7a4eef1d1aeac829f44c9efd9c2625
James Dai <james.dai@metadataconsulting.co.uk>
2021-04-26 16:19:36 +0100

    01: Basic Routing

--- 01-basic-routing/src/components/App.js
+++ 01-basic-routing/src/components/App.js
@@ -1,23 +1,37 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
 
-// import route Components here
+import Hello from './HelloComponent';
+import About from './AboutComponent';
+import Books from './BooksComponent';
 
+// import route Components here
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect
+} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="container">
-          <ul>
-            <li><a href="">Hello</a></li>
-            <li><a href="">About</a></li>
-            <li><a href="">Books</a></li>
-          </ul>
-          <hr/>
-          {/* Routes will go here */}
-      </div>
-  </div>
+      <Router>
+        <div className="App">
+          <div className="container">
+            <ul>
+              <li><Link to="/hello">Hello</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/books">Books</Link></li>
+            </ul>
+            <hr/>
+            <Route path="/hello" component={Hello} />
+            <Route path="/about" component={About} />
+            <Route path="/books" component={Books} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
